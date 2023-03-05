96aabcdc814f7af088b4846cf8df083667097443
Pawan <paletipawankumar@gmail.com>
2023-03-05 11:22:58 +0530

    Adding initial files to the repo

---
+++ app/src/main/java/com/example/registration/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.registration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText username = (EditText) findViewById(R.id.Username);
+
+        MaterialButton mtrbutton = (MaterialButton) findViewById(R.id.signupbutton);
+
+        mtrbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username1 = username.getText().toString();
+                Toast.makeText(MainActivity.this,"Sign In Successfull " +username1,Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
